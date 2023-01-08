c91fe3f867b96c0bd49f530e2af5b7a44e35a037
arendscm <christopher-maximilian.arends@charite.de>
2023-01-08 15:45:01 +0100

    08012023

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
 import pandas as pd 
-df = pd.read_csv("/home/arendscm/data-science-environment/data/seq_data.csv")
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sn
 
-def my_function():
-    pass
+#import sequencing data
+df = pd.read_csv("/home/arendscm/data-science-environment/data/raw/seq_data.csv")
+#subset data frame
+subdf = df[["Sample","Gene"]]
+#transform into matrix
+subdfdummy = pd.get_dummies(subdf,columns=['Gene']) #introduce dummies
+df_mut = subdfdummy.groupby(by=['Sample']).sum() #sum by Sample
+coocc = df_mut.T.dot(df_mut) # cooccurrence matrix
+
+print(sn.heatmap(coocc,annot=True))
+
+
+xaxis = np.array([2, 8])
+
+# Y axis parameter:
+yaxis = np.array([4, 9])
+
+plt.plot(xaxis, yaxis)
+plt.show()
